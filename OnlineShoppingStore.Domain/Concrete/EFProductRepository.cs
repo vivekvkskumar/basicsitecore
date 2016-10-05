28451cac65fdfdb3316adac5d5f9e034f432a65b
Vivek <kumar.vivek@indiacansolutions.com>
2016-10-05 16:26:09 +0530

    Added project solution and files.

---
+++ OnlineShoppingStore.Domain/Concrete/EFProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using OnlineShoppingStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShoppingStore.Domain.Entities;
+
+namespace OnlineShoppingStore.Domain.Concrete
+{
+    public class EFProductRepository : IProductRepository
+    {
+        private readonly EFDbContext context = new EFDbContext();
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                { return context.Products; }
+            }
+        }
+    }
+}
